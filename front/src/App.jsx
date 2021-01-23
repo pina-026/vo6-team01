00b5fbd2f7d9073b80e524acb93e52b28f69d074
yuma <yuumaneoahuro@gmail.com>
2021-01-23 23:36:49 +0900

    ログイン画面

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,37 @@
 import SignUp from './Login'
+import SignIn from './signin'
+import React from 'react';
+import { BrowserRouter, Route,Link } from 'react-router-dom';
 
-function App() {
+const PageSignup = () => {
   return (
-    <div className="App">
-      <SignUp />
+    <div className="PageSignup">
+    <SignUp />
     </div>
   )
 }
+const PageSignin =()=>{
+  return(
+    <div className="PageSignin">
+    <SignIn />
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageSignup} />
+          <Route path="/pageSignin"  component={PageSignin} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+
+
 
 export default App
--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -5,13 +5,13 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import TextField from '@material-ui/core/TextField'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
-import Link from '@material-ui/core/Link'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import  {Link} from 'react-router-dom';
 
 function Copyright() {
   return (
@@ -93,6 +93,7 @@ export default function SignUp() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+
               />
             </Grid>
             <Grid item xs={12}>
@@ -126,7 +127,9 @@ export default function SignUp() {
           <Grid container justify="flex-end">
             <Grid item>
               <Link href="#" variant="body2">
+                <Link to='/PageSignin'>
                 Already have an account? Sign in
+                </Link>
               </Link>
             </Grid>
           </Grid>
